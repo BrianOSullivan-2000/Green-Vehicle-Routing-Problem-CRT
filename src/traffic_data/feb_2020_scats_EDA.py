44b20930f6ae0a7403a323d768bd4bb846c43682
kfinucane <kate.finucane@ucdconnect.ie>
2021-11-19 20:56:13 +0000

    updated variable name and minor plot changes

--- src/traffic_data/feb_2020_scats_EDA.py
+++ src/traffic_data/feb_2020_scats_EDA.py
@@ -13,15 +13,15 @@ feb_traffic_data = pd.read_pickle(".\\data\\scats_feb2020_processed_data.pkl")
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # look at summary stats by hour
-summary_by_hr = feb_traffic_data.groupby("Hour_in_Day")["Sum_Volume"].describe()
+summary_by_hr = feb_traffic_data.groupby("Hour_in_Day")["All_Detector_Vol"].describe()
 
 # summary stats by hour and by weekday/weekend
-summary_by_hr_wdwe = feb_traffic_data.groupby(["Hour_in_Day", "Day_Type"])["Sum_Volume"].describe()
+summary_by_hr_wdwe = feb_traffic_data.groupby(["Hour_in_Day", "Day_Type"])["All_Detector_Vol"].describe()
 summary_by_hr_wdwe = summary_by_hr_wdwe.reset_index(level="Day_Type")
 summary_by_hr_wdwe.index = summary_by_hr_wdwe.index.astype(int)
 
 # summary stats by hour and by day
-summary_by_hr_d = feb_traffic_data.groupby(["Hour_in_Day", "Day_in_Week"])["Sum_Volume"].describe()
+summary_by_hr_d = feb_traffic_data.groupby(["Hour_in_Day", "Day_in_Week"])["All_Detector_Vol"].describe()
 summary_by_hr_d = summary_by_hr_d.reset_index(level="Day_in_Week")
 summary_by_hr_d.index = summary_by_hr_d.index.astype(int)
 
@@ -30,8 +30,8 @@ fig, ax = plt.subplots()
 summary_by_hr_wdwe.groupby("Day_Type")["mean"].plot()
 plt.xlim(0, 25)
 plt.xlabel("Hour in Day")
-plt.ylabel("Mean Vehicle Count")
-plt.title("February 2020: Mean Vehicle Count by Hour in Day")
+plt.ylabel("Mean Site Vehicle Count")
+plt.title("February 2020: Mean Site Vehicle Count by Hour in Day")
 plt.grid(True)
 plt.legend(title="Day Type")
 
@@ -50,8 +50,8 @@ fig, ax = plt.subplots()
 summary_by_hr_d.groupby("Day_in_Week")["mean"].plot()
 plt.xlim(0, 25)
 plt.xlabel("Hour in Day")
-plt.ylabel("Mean Vehicle Count")
-plt.title("February 2020: Mean Vehicle Count by Hour in Day")
+plt.ylabel("Mean Site Vehicle Count")
+plt.title("February 2020: Mean Site Vehicle Count by Hour in Day")
 plt.grid(True)
 plt.legend(title="Day in Week")
 
@@ -62,8 +62,8 @@ plt.legend(title="Day in Week")
 wd_feb_traffic = feb_traffic_data[feb_traffic_data["Day_Type"] == "WD"].copy()
 
 # range normalise b/t 1 and 0
-wd_norm_traffic_val = (wd_feb_traffic["Sum_Volume"] - min(wd_feb_traffic["Sum_Volume"])) /\
-                      (max(wd_feb_traffic["Sum_Volume"]) - min(wd_feb_traffic["Sum_Volume"]))
+wd_norm_traffic_val = (wd_feb_traffic["All_Detector_Vol"] - min(wd_feb_traffic["All_Detector_Vol"])) /\
+                      (max(wd_feb_traffic["All_Detector_Vol"]) - min(wd_feb_traffic["All_Detector_Vol"]))
 
 wd_feb_traffic["Norm_Vol_WD"] = wd_norm_traffic_val
 
@@ -71,8 +71,8 @@ wd_feb_traffic["Norm_Vol_WD"] = wd_norm_traffic_val
 we_feb_traffic = feb_traffic_data[feb_traffic_data["Day_Type"] == "WE"].copy()
 
 # range normalise b/t 1 and 0
-we_feb_traffic["Norm_Traffic_Val"] = (we_feb_traffic["Sum_Volume"] - min(we_feb_traffic["Sum_Volume"])) /\
-                                     (max(we_feb_traffic["Sum_Volume"]) - min(we_feb_traffic["Sum_Volume"]))
+we_feb_traffic["Norm_Traffic_Val"] = (we_feb_traffic["All_Detector_Vol"] - min(we_feb_traffic["All_Detector_Vol"])) /\
+                                     (max(we_feb_traffic["All_Detector_Vol"]) - min(we_feb_traffic["All_Detector_Vol"]))
 
 # TODO: update with further stats if needed
 # TODO: make table for report
